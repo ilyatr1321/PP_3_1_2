8845ae92800ec670c53a0b75ee12ff3239b53415
ilyatr1321 <ilyatr1321@gmail.com>
2022-11-03 15:13:19 +0300

    Initial commit

---
+++ src/main/java/ru/trofimov/pp_3_1_2/service/UserServiceImpl.java
@@ -0,0 +1,58 @@
+package com.service;
+
+import com.dao.UserDAO;
+import com.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    private final UserDAO userDAO;
+
+    @Autowired
+    public UserServiceImpl(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> getAllUsers() {
+        return userDAO.getAllUsers();
+    }
+
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        userDAO.saveUser(user);
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(User user, int id) {
+        userDAO.updateUser(user, id);
+    }
+
+    @Override
+    @Transactional
+    public User getUserById(int id) {
+        return userDAO.getUserById(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(int id) {
+        userDAO.deleteUser(id);
+    }
+
+    @Override
+    @Transactional
+    public User getUser(int id) {
+        return userDAO.getUserById(id);
+    }
+}
